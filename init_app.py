918c226eefcea07c6dada64811aac2ba6ef5c809
Rishi Suri <rkart@terpmail.umd.edu>
2026-04-17 12:03:21 -0400

    cleanup dashboard

--- init_app.py
+++ init_app.py
@@ -11,103 +11,38 @@ def _():
     from nilearn import datasets
     from ipyniivue import NiiVue
     from pathlib import Path
+    import nibabel as nib
 
     return NiiVue, Path, datasets, mo
 
 
-@app.cell
-def _():
-    import nibabel as nib
-
-    return (nib,)
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    # Interactive Dashboard
+    """)
+    return
 
 
 @app.cell
 def _(datasets):
     fsaverage = datasets.fetch_surf_fsaverage(mesh="fsaverage")
-    fsaverage
     return (fsaverage,)
 
 
-@app.cell
-def _(nib):
-    import numpy as np
-
-    def annot_to_label_gii(annot_path, out_path):
-        labels, ctab, names = nib.freesurfer.read_annot(annot_path)
-    
-        label_table = nib.gifti.GiftiLabelTable()
-        for i, (name, color) in enumerate(zip(names, ctab)):
-            gl = nib.gifti.GiftiLabel(
-                key=i,
-                red=color[0]/255,
-                green=color[1]/255,
-                blue=color[2]/255,
-                alpha=1.0
-            )
-            gl.label = name.decode() if isinstance(name, bytes) else name
-            label_table.labels.append(gl)
-
-        arr = nib.gifti.GiftiDataArray(
-            data=labels.astype(np.int32),
-            intent=nib.nifti1.intent_codes['NIFTI_INTENT_LABEL'],
-            datatype='NIFTI_TYPE_INT32'
-        )
-        img = nib.gifti.GiftiImage(darrays=[arr], labeltable=label_table)
-        nib.save(img, out_path)
-
-    annot_to_label_gii("data/atlases/lh.HCPMMP1.annot", "data/atlases/lh.HCPMMP1.label.gii")
-    annot_to_label_gii("data/atlases/rh.HCPMMP1.annot", "data/atlases/rh.HCPMMP1.label.gii")
-    return
-
-
-@app.cell
-def _():
-    """
-
-    nv = NiiVue()
-    nv.load_meshes([
-        {"path": Path(fsaverage["infl_left"]), "rgba255": [64, 22, 222, 255]}, {"path": Path(fsaverage["infl_right"]), "rgba255": [222, 64, 22, 255]}
-    ]) 
-    nv
-
-    """
-    """
-    nv = NiiVue()
-    nv.load_meshes([
-        {
-            "path": Path(fsaverage["infl_left"]),
-            "layers": [{"path": Path("data/atlases/lh.HCPMMP1.label.gii")}]
-        },
-        {
-            "path": Path(fsaverage["infl_right"]),
-            "layers": [{"path": Path("data/atlases/rh.HCPMMP1.label.gii")}]
-        },
-    ])
-    nv
-    """
-    return
-
-
-@app.cell
-def _(fsaverage):
-    from nilearn import plotting
-
-    plotting.plot_surf_roi(
-        surf_mesh=fsaverage["infl_left"],
-        roi_map="data/atlases/lh.HCPMMP1.annot",
-        hemi="left",
-        view="lateral",
-        bg_map=fsaverage["sulc_left"],
-    )
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ## Plot Glasser Atlas
+    """)
     return
 
 
 @app.cell
 def _(mo):
     hemi = mo.ui.radio(
-        options=["Left", "Right", "Both"],
-        value="Both",
+        options=["Left", "Right"],
+        value = "Left",
         label="Hemisphere"
     )
     hemi
@@ -121,19 +56,14 @@ def _(mo):
 
 
 @app.cell
-def _():
-    return
-
-
-@app.cell
 def _(NiiVue, Path, fsaverage, hemi, set_label):
     meshes = []
-    if hemi.value in ("Left", "Both"):
+    if hemi.value in ("Left"):
         meshes.append({
             "path": Path(fsaverage["infl_left"]),
             "layers": [{"path": Path("data/atlases/lh.HCPMMP1.label.gii")}]
         })
-    if hemi.value in ("Right", "Both"):
+    if hemi.value in ("Right"):
         meshes.append({
             "path": Path(fsaverage["infl_right"]),
             "layers": [{"path": Path("data/atlases/rh.HCPMMP1.label.gii")}]
